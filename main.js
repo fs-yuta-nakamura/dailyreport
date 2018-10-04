75d66be62654a83ad974f991b618eac388e8d45e
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2018-10-04 11:54:01 +0900

    Working version of vue_app

--- main.js
+++ main.js
@@ -41,35 +41,8 @@ var app = new Vue({
     result: false
   },
   computed: {
-    commitLength: function() {
-      return this.commitReview.replace(/[\s\n]+/g, "").length
-    },
-    idenshiLength: function() {
-      return this.idenshiReview.replace(/[\s\n]+/g, "").length
-    },
-    calendarLength: function() {
-      return this.calendar.replace(/[\s\n]+/g, "").length
-    },
-    jsDate: function(){
-      return new Date(this.date);
-    },
-    month: function(){ 
-      return this.jsDate.getMonth() + 1 },
-    day: function(){
-      return this.jsDate.getDate() },
-    week: function(){
-      return '日月火水木金土'[this.day] }
-  },
-  methods: {
-    lengthNippo: function(nippo) {
-      var startIndex = nippo.indexOf("```") + 3;
-      var lastIndex = nippo.lastIndexOf("```") - 1;
-      var mainContent = nippo.substring(startIndex, lastIndex);
-      mainContent = mainContent.replace(/[\s\n]+/g, "");
-      return mainContent.length;
-    },
-    makeNippo: function(){
-    var nippo = `お疲れ様です。
+    nippo: function (){
+     nippo_text = `お疲れ様です。
 中村です。
 ${this.month}月${this.day}日(${this.week})の日報です。
 
@@ -120,14 +93,49 @@ XXX字
 https://docs.google.com/spreadsheets/d/1kgZgrhEhfshn5jZQ2rXrJ1C59IvfoRnznHBZ0WJ1qdY/edit#gid=1451509297
   \`\`\` `
 
-      nippo = nippo.replace("XXX", this.lengthNippo(nippo));
-      copyText(nippo);
-
-      this.result = true;
-
-      document.getElementById("result").scrollIntoView({behavior: "smooth", block: "start"});
-      return nippo
+     return  nippo_text.replace("XXX", this.lengthNippo(nippo_text));
     },
+    commitLength: function() {
+      return this.commitReview.replace(/[\s\n]+/g, "").length
+    },
+    idenshiLength: function() {
+      return this.idenshiReview.replace(/[\s\n]+/g, "").length
+    },
+    calendarLength: function() {
+      return this.calendar.replace(/[\s\n]+/g, "").length
+    },
+    jsDate: function(){
+      return new Date(this.date);
+    },
+    month: function(){ 
+      return this.jsDate.getMonth() + 1 },
+    day: function(){
+      return this.jsDate.getDate() },
+    week: function(){
+      return '日月火水木金土'[this.day] }
+  },
+  methods: {
+    lengthNippo: function(nippo) {
+      var startIndex = nippo.indexOf("```") + 3;
+      var lastIndex = nippo.lastIndexOf("```") - 1;
+      var mainContent = nippo.substring(startIndex, lastIndex);
+      mainContent = mainContent.replace(/[\s\n]+/g, "");
+      return mainContent.length;
+    },
+    makeNippo: function(){
+      copyText(this.nippo);
+      this.result = true;
+      this.$nextTick(function(){
+        document.getElementById("result").scrollIntoView({behavior: "smooth", block: "start"});
+      })
+    },
+    saveData: function(){
+      localStorage.setItem("datalist", JSON.stringify(this.$data)) 
+    },
+    loadData: function(){
+      savedData = JSON.parse( localStorage.getItem("datalist") ) || [];
+      app.commitReview = savedData.commitReview;
+    }
   } 
 })
 /*
@@ -153,109 +161,4 @@ Vue.component('text-review', {
   `
 }
 )
-
-new Vue({el: "#content-area"})
-
-setDefaultDate();
-  
-var makeNippo = function(){
-  var date = new Date($('#date').val());
-  var month = date.getMonth() + 1;
-  var day = date.getDate();
-  var week = '日月火水木金土'[date.getDay()];
-  var commitNum = $('#commitment').val();
-  var commitReview = $("#commit-review").val().trim();
-  var todayIdenshi = $("#idenshi").val();
-  var idenshiReview = $("#idenshi-review").val().trim();
-  var tomorrowIdenshi = $("#tmr-idenshi").val()
-  var calendar = $("#calendar").val()
-
-  var nippo = `お疲れ様です。
-  中村です。
-  ${month}月${day}日(${week})の日報です。
-
-  岩本さん、楢木さん
-  お手すきの際にご確認よろしくお願い致します。
-  @岩本 俊亮 @楢木 稜二
-
-  岩本HDの皆様よろしくお願いします。
-  @襲田 絢香 @瀧口 賢治
-  \`\`\`
-  中村優太＠fsに1番貢献している人間になる
-
-
-  ■月次目標
-  「時間の使い方を改善する」
-
-  ・コミットメント
-  1. すべてのタスクの期日を明確に切る
-  2. タスクの期日は必ず守る
-  3. 時間の使い方を計画する
-  4. 時間あたりの生産性を意識する
-  5. 時間のポートフォリオを意識する
-
-  ・振り返り (${commitNum})
-  ${commitReview}
-
-
-  ■本日意識した遺伝志
-  「${todayIdenshi}」
-
-  ・振り返り
-  ${idenshiReview}
-
-
-  ■明日意識する遺伝志
-  「${tomorrowIdenshi}」
-
-
-  ■本日の流れ
-  ${calendar}
-
-
-  ■文字数
-  XXX字
-
-
-  ■【シラバス】学習進捗確認表
-  https://docs.google.com/spreadsheets/d/1kgZgrhEhfshn5jZQ2rXrJ1C59IvfoRnznHBZ0WJ1qdY/edit#gid=1451509297
-  \`\`\` `
-
-  nippo = nippo.replace("XXX", lengthNippo(nippo));
-  copyText(nippo);
-  $("#result").show();
-  $("#result-info").fadeIn('slow');
-
-  var result = document.getElementById("result");
-  var resultText = document.getElementById("result-text");
-  resultText.innerText = nippo;
-  result.scrollIntoView({behavior: "smooth", block: "start"});
-}
-
-
-  var lengthNippo = function(nippo) {
-  var startIndex = nippo.indexOf("```") + 3;
-  var lastIndex = nippo.lastIndexOf("```") - 1;
-  var mainContent = nippo.substring(startIndex, lastIndex);
-  mainContent = mainContent.replace(/[\s\n]+/g, "");
-  return mainContent.length;
-}
-
-
-function copyText(text){
-  var ta = document.createElement("textarea")
-  ta.value = text
-  document.body.appendChild(ta)
-  ta.select()
-  document.execCommand("copy")
-  ta.parentElement.removeChild(ta)
-}
-
-// main functions
-$("#make").click(function(){ makeNippo() })
-$("#make").keypress(function(e){
-  if(e.keycode == 13){
-    makeNippo; 
-  }
-});
 */
