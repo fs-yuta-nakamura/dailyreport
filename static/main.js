b2a294602d3c329617a7f47b222db2d85fb1d1a8
Yuta Nakamura <yuta.nakamura@f-scratch.com>
2018-11-05 14:47:59 +0000

    change commit

--- static/main.js
+++ static/main.js
@@ -7,10 +7,10 @@ firebase.initializeApp({
 // Initialize Cloud Firestore through Firebase
 var db = firebase.firestore();
 
-function copyText(text){
-  var ta = document.createElement("textarea")
-  ta.value = text
-  document.body.appendChild(ta)
+function copyText(text) {
+  var ta = document.createElement("textarea");
+  ta.value = text;
+  document.body.appendChild(ta);
   ta.select();
   document.execCommand("copy");
   ta.parentElement.removeChild(ta);
@@ -22,66 +22,71 @@ function copyText(text){
   });
 }
 
-var setDefaultDate = function(){
+var setDefaultDate = function() {
   // set today's date as default date'
   var date = new Date();
   var year = date.getFullYear();
   var month = date.getMonth() + 1;
-  var day ;
+  var day;
   var hours = date.getHours();
-  console.log(hours)
+  console.log(hours);
 
-  if (hours > 0 && hours < 2){
-    day = date.getDate() - 1; 
+  if (hours > 0 && hours < 2) {
+    day = date.getDate() - 1;
   } else {
     day = date.getDate();
   }
 
-  var toTwoDigits = function(num, digit){
-    num  += '';
-    if (num.length < digit){
-      num = '0' + num ;
+  var toTwoDigits = function(num, digit) {
+    num += "";
+    if (num.length < digit) {
+      num = "0" + num;
     }
     return num;
-  }
+  };
   var yyyy = toTwoDigits(year, 4);
   var mm = toTwoDigits(month, 2);
   var dd = toTwoDigits(day, 2);
   var ymd = yyyy + "-" + mm + "-" + dd;
-  return ymd
-}
+  return ymd;
+};
 
-var downloadURL = function(){
+var downloadURL = function() {
   var content = "あいうえお";
-  var blob = new Blob([ content ], { "type" : "text/plain" });
+  var blob = new Blob([content], { type: "text/plain" });
   var downloadLink = window.URL.createObjectURL(blob);
   document.getElementById("download").href = downloadLink;
-}
+};
 
-
-function iftttWebhook(eventName, payload){
-  var postUrl = "/ifttt"
+function iftttWebhook(eventName, payload) {
+  var postUrl = "/ifttt";
   console.log(payload),
-  axios.post(postUrl, payload)
-    .then(function (response) {
-      console.log(response);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+    axios
+      .post(postUrl, payload)
+      .then(function(response) {
+        console.log(response);
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
 }
 
 // initialize materialize
-document.addEventListener('DOMContentLoaded', function() {
-	var elems = document.querySelectorAll('select');
-	var instances = M.FormSelect.init(elems);
+//document.addEventListener('DOMContentLoaded', function() {
+//var elems = document.querySelectorAll('select');
+//var instances = M.FormSelect.init(elems);
+//});
+
+//materialize modal
+document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".modal");
+  var instances = M.Modal.init(elems, options);
 });
 
 var app = new Vue({
   el: "#app-main",
   data: {
-    nippo:
-    {
+    nippo: {
       date: setDefaultDate(),
       commitNum: "",
       commitReview: "",
@@ -89,17 +94,23 @@ var app = new Vue({
       idenshiReview: "",
       tomorrowIdenshi: "",
       calendar: "",
-      commitmemnts: ["時間を理由に挑戦をやめない","できる、できないではなくやる方法を考える","学びを転化する","自分から機会を取りに行く","時間の管理を細かくする" ],
+      commitmemnts: [
+        "自分に負荷をかける",
+        "集中力を高める",
+        "成長機会に貪欲になる",
+        "自分から機会を取りに行く",
+        "積極的な発信を行う"
+      ],
       content: ""
     },
     result: false
   },
-  created: function(){
+  created: function() {
     this.loadFromFirebase();
   },
   computed: {
-    formatedNippo: function (){
-     nippo_text =   `お疲れ様です。
+    formatedNippo: function() {
+      nippo_text = `お疲れ様です。
 中村です。
 ${this.month}月${this.day}日(${this.week})の日報です。
 
@@ -146,30 +157,38 @@ XXX字
 
 
 ■【シラバス】学習進捗確認表
-https://docs.google.com/spreadsheets/d/1kgZgrhEhfshn5jZQ2rXrJ1C59IvfoRnznHBZ0WJ1qdY/edit#gid=1451509297\`\`\` `
+https://docs.google.com/spreadsheets/d/1kgZgrhEhfshn5jZQ2rXrJ1C59IvfoRnznHBZ0WJ1qdY/edit#gid=1451509297\`\`\` `;
 
-      return  nippo_text.replace("XXX", this.lengthNippo(nippo_text));
+      return nippo_text.replace("XXX", this.lengthNippo(nippo_text));
     },
     commitLength: function() {
-      return this.nippo.commitReview.replace(/[\s\n]+/g, "").length
+      return this.nippo.commitReview.replace(/[\s\n]+/g, "").length;
     },
     idenshiLength: function() {
-      return this.nippo.idenshiReview.replace(/[\s\n]+/g, "").length
+      return this.nippo.idenshiReview.replace(/[\s\n]+/g, "").length;
     },
     calendarLength: function() {
-      return this.nippo.calendar.replace(/[\s\n]+/g, "").length
+      return this.nippo.calendar.replace(/[\s\n]+/g, "").length;
     },
-    jsDate: function(){
+    jsDate: function() {
       return new Date(this.nippo.date);
     },
-    month: function(){ 
-      return this.jsDate.getMonth() + 1 },
-    day: function(){
-      return this.jsDate.getDate() },
-    week: function(){
-      return '日月火水木金土'[this.jsDate.getDay()] },
-    file_name: function(){
-      return "" + this.nippo.date.slice(5, 7) + this.nippo.date.slice(8, 10) + "_日報";
+    month: function() {
+      return this.jsDate.getMonth() + 1;
+    },
+    day: function() {
+      return this.jsDate.getDate();
+    },
+    week: function() {
+      return "日月火水木金土"[this.jsDate.getDay()];
+    },
+    file_name: function() {
+      return (
+        "" +
+        this.nippo.date.slice(5, 7) +
+        this.nippo.date.slice(8, 10) +
+        "_日報"
+      );
     }
   },
   methods: {
@@ -180,69 +199,76 @@ https://docs.google.com/spreadsheets/d/1kgZgrhEhfshn5jZQ2rXrJ1C59IvfoRnznHBZ0WJ1
       mainContent = mainContent.replace(/[\s\n]+/g, "");
       return mainContent.length;
     },
-    makeNippo: function(){
+    makeNippo: function() {
       copyText(this.formatedNippo);
       this.result = true;
-      this.$nextTick(function(){
-        document.getElementById("result").scrollIntoView({behavior: "smooth", block: "start"});
-      })
+      this.$nextTick(function() {
+        document
+          .getElementById("result")
+          .scrollIntoView({ behavior: "smooth", block: "start" });
+      });
       // send to ifttt
-      iftttWebhook("nippo_submit",{
-        "value1": this.nippo.date,
-        "value2": this.formatedNippo
+      iftttWebhook("nippo_submit", {
+        value1: this.nippo.date,
+        value2: this.formatedNippo
       });
       this.saveToFirebase();
     },
-    saveData: function(){
-      localStorage.setItem("datalist", JSON.stringify(this.nippo)) 
+    saveData: function() {
+      localStorage.setItem("datalist", JSON.stringify(this.nippo));
     },
-    loadData: function(){
-      savedData = JSON.parse( localStorage.getItem("datalist")  || '[]');
+    loadData: function() {
+      savedData = JSON.parse(localStorage.getItem("datalist") || "[]");
       console.log(savedData);
       app.nippo = savedData;
     },
-    download: function(){
-      var blob = new Blob([ this.formatedNippo ], { "type" : "text/plain" });
+    download: function() {
+      var blob = new Blob([this.formatedNippo], { type: "text/plain" });
       var downloadLink = window.URL.createObjectURL(blob);
-      this.$nextTick(function(){
-        document.getElementById("download").href = downloadLink;    
+      this.$nextTick(function() {
+        document.getElementById("download").href = downloadLink;
       });
     },
-    saveToFirebase: function(){
-      db.collection("nippos").doc(this.nippo.date).set({
-        timestamp: new Date(),
-        nippo: this.nippo,
-        formated: this.formatedNippo
-      })
-        .then(function(){
+    saveToFirebase: function() {
+      db.collection("nippos")
+        .doc(this.nippo.date)
+        .set({
+          timestamp: new Date(),
+          nippo: this.nippo,
+          formated: this.formatedNippo
+        })
+        .then(function() {
           console.log("saved to Firebase");
         })
-        .then(function(){
+        .then(function() {
           M.toast({
             html: "保存しました",
             displayLength: 2000,
             inDuration: 500,
             outDuration: 375
-          })
-        })  
+          });
+        });
     },
-    loadFromFirebase: function(){
-      db.collection("nippos").doc(this.nippo.date).get().then((doc) => {
-        if (doc.exists) {
-          console.log("Document data:", doc.data());
-          this.nippo = doc.data().nippo;
-        }
-      })
-        .then(function(){
+    loadFromFirebase: function() {
+      db.collection("nippos")
+        .doc(this.nippo.date)
+        .get()
+        .then(doc => {
+          if (doc.exists) {
+            console.log("Document data:", doc.data());
+            this.nippo = doc.data().nippo;
+          }
+        })
+        .then(function() {
           M.toast({
             html: "ロードしました",
             displayLength: 2000,
             inDuration: 500,
             outDuration: 375
-          })
-        })
+          });
+        });
     }
   }
-})
+});
 // add shortcut for saving data
 shortcut.add("Ctrl+S", app.saveToFirebase);
